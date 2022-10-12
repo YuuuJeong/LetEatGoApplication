63439ab68f759dff694623c95b17fece0608ebef
신 우현 <woo_hyun_1@naver.com>
2022-10-12 19:03:42 +0900

    Connect Auth and Main

---
+++ frontend/screens/MainScreen/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Home({ navigation, route }) {
+  return (
+    <View>
+      <Text>This is Home screen !</Text>
+    </View>
+  );
+}
+
+export default Home;
