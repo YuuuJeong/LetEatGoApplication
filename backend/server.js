ad345934118239314272b04120c1251515e7d84e
신 우현 <woo_hyun_1@naver.com>
2022-11-04 20:10:58 +0900

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express');
-const session = require('express-session');
-const morgan = require('morgan');
-const cors = require('cors');
-const db = require('./models');
+const express = require("express");
+const session = require("express-session");
+const morgan = require("morgan");
+const cors = require("cors");
+const db = require("./models");
 
-const { sequelize } = require('./models/index');
-const userRouter = require('./routes/user');
-const checkRouter = require('./routes/check');
-const recipeRouter = require('./routes/recipe');
-const testRouter = require('./routes/test');
-const searchRouter = require('./routes/search');
+const { sequelize } = require("./models/index");
+const userRouter = require("./routes/user");
+const checkRouter = require("./routes/check");
+const recipeRouter = require("./routes/recipe");
+// const testRouter = require('./routes/test');
+const searchRouter = require("./routes/search");
 const app = express();
 
-app.set('port', process.env.PORT || 80);
+app.set("port", process.env.PORT || 80);
 
 app.use(morgan("dev"));
 
@@ -23,13 +23,16 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-db.sequelize.sync().then(() => {
-        console.log('db connect success');
-    }).catch(console.error);
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("db connect success");
+  })
+  .catch(console.error);
 
-app.use(session({ secret: 'SECRET' }));
+app.use(session({ secret: "SECRET" }));
 
 app.use(
   session({
@@ -41,19 +44,19 @@ app.use(
       secure: false,
     },
   })
-);    
+);
 
-app.use('/', userRouter);
-app.use('/', recipeRouter);
-app.use('/check', checkRouter);
-app.use('/test', testRouter);
-app.use('/search', searchRouter);
+app.use("/", userRouter);
+app.use("/", recipeRouter);
+app.use("/check", checkRouter);
+// app.use("/test", testRouter);
+app.use("/search", searchRouter);
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(err.status || 500).send(err.message);
-  });
-  
-  app.listen(app.get("port"), () => {
-    console.log(app.get("port"), "번 포트에서 대기중");
-  });
+  console.error(err);
+  res.status(err.status || 500).send(err.message);
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
